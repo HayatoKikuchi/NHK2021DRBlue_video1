e8f2e99786deabdc9a658fcadc61de1ca38718f0
菊池隼 <e51190405b@edu.teu.ac.jp>
2021-03-29 22:23:01 +0900

    壁越え出来たよ！

--- NHK2021_DRbluePlan_manual/NHK2021_DRbluePlan_manual.ino
+++ NHK2021_DRbluePlan_manual/NHK2021_DRbluePlan_manual.ino
@@ -20,16 +20,17 @@ phaseCounter encX(1);
 phaseCounter encY(2);
 DRBlue DR(&lpms, &encX, &encY); //DRのセットアップなどを行う
 
-PID posiZ_pid(0.0,0.0,0.0,INT_TIME);
+PID posiZ_pid(4.0, 0.0, 1.0,INT_TIME);
 ManualControl ManualCon(&posiZ_pid); //メカナムの速度制御
 Controller Con(&SERIAL_XBEE); //dualshock4
 myLCDclass lcd(&SERIAL_LCD);
 
-RoboClaw roboclaw(&SERIAL_ROBOCLAW,1000);
-DRwall wall_1(PIN_SW_WALL_1,PIN_SUPPORT_WHEEL_1,ADR_MD_WHEE_1,&roboclaw); // 壁越えの制御を行う
-DRwall wall_2(PIN_SW_WALL_2,PIN_SUPPORT_WHEEL_2,ADR_MD_WHEE_2,&roboclaw); //　↓
-DRwall wall_3(PIN_SW_WALL_3,PIN_SUPPORT_WHEEL_3,ADR_MD_WHEE_3,&roboclaw); //　↓
-DRwall wall_4(PIN_SW_WALL_4,PIN_SUPPORT_WHEEL_4,ADR_MD_WHEE_4,&roboclaw); //　↓
+RoboClaw roboclawR(&SERIAL_ROBOCLAW_R,1000);
+RoboClaw roboclawL(&SERIAL_ROBOCLAW_L,1000);
+DRwall wall_1(PIN_SW_WALL_1,PIN_SUPPORT_WHEEL_1,ADR_MD_WHEE_1,&roboclawL); // 壁越えの制御を行う
+DRwall wall_2(PIN_SW_WALL_2,PIN_SUPPORT_WHEEL_2,ADR_MD_WHEE_2,&roboclawL); //　↓
+DRwall wall_3(PIN_SW_WALL_3,PIN_SUPPORT_WHEEL_3,ADR_MD_WHEE_3,&roboclawR); //　↓
+DRwall wall_4(PIN_SW_WALL_4,PIN_SUPPORT_WHEEL_4,ADR_MD_WHEE_4,&roboclawR); //　↓
 
 DRexpand expand_right(PIN_SW_EXPAND_RIGHT,PIN_SUPPORT_RIGHT); // 展開機構
 DRexpand expand_left(PIN_SW_EXPAMD_LEFT,PIN_SUPPORT_LEFT);    // ↓
@@ -55,54 +56,67 @@ bool button_LEFT = false;
 
 /****変数****/
 bool flag_10ms  = false;
+bool flag_100ms = false;
 
 int expand_mode; // 展開方法に関する変数
 int robot_velocity_mode; //足回りの走行仕様について
 int wall_mode; // 壁越え機構に関する変数
 int turning_mode; //ロボットの旋回モード
 
-void dipSetup(){  
-  if(dipsw_state & DIP1){
+double globalVelZ = 0.0;
+
+void dipSetup()
+{  
+  if(dipsw_state & DIP1)
+  {
     expand_mode = 2; // 2段階目をコントローラから指示する
     digitalWrite(PIN_LED_1,HIGH);
   }  
-  else{
+  else
+  {
     digitalWrite(PIN_LED_1,LOW);
     expand_mode = 1; // 2段階目をリミットスイッチで判断
   } 
 
-  if(dipsw_state & DIP2){
-    
+  if(dipsw_state & DIP2)
+  {  
     robot_velocity_mode = 1; //X方向にのみ進む
     digitalWrite(PIN_LED_2,HIGH);
   }
-  else{
+  else
+  {
     robot_velocity_mode = 2; //全方向移動
     digitalWrite(PIN_LED_2,LOW);
   }
 
-  if(dipsw_state & DIP3){
+  if(dipsw_state & DIP3)
+  {
     wall_mode = 1; //テストモード
     digitalWrite(PIN_LED_3,HIGH);
   }
-  else{
+  else
+  {
     wall_mode = 2; //通常モード
     digitalWrite(PIN_LED_3,LOW);
   }
 
-  if(dipsw_state & DIP4){
+  if(dipsw_state & DIP4)
+  {
     turning_mode = 1; // 角度PID無し
     digitalWrite(PIN_LED_4,HIGH);
     digitalWrite(PIN_LED_ENC,LOW);
   }
-  else{
+  else
+  {
     turning_mode = 2; // 角度PID有
     digitalWrite(PIN_LED_4,LOW);
     digitalWrite(PIN_LED_ENC,HIGH);
   }
 }
 
-void radianPID_setup(){
+double serial_num;
+void radianPID_setup(bool flag)
+{
   static int pid_setting_mode = 1;
   static double Kp = 0.0, Ki = 0.0, Kd = 0.0;
   static bool init_kp = true, init_ki = true, init_kd = true;
@@ -113,8 +127,11 @@ void radianPID_setup(){
   if(pid_setting_mode == 0) pid_setting_mode = 3;
   else if(pid_setting_mode == 4) pid_setting_mode = 1;
   
-  if(flag_lcd){
+  if(flag_lcd && flag)
+  { 
+    lcd.clear_display();
     lcd.write_str("RadianPID Setting",LINE_1,1);
+    lcd.write_str("CMD ",LINE_4,1);
     flag_lcd = false;
   }
 
@@ -123,60 +140,79 @@ void radianPID_setup(){
   case 1:
     init_ki = true;
     init_kd = true;
-    if(init_kp){
-      lcd.write_str("Kp ",LINE_3,1); //3コマ使用
-      enc.setEncCount((int)(10.0 * Kp));
+    if(init_kp)
+    { 
+      if(flag) lcd.write_str("Kp ",LINE_3,1); //3コマ使用
+      enc.setEncCount((int)(10.0 * posiZ_pid.Kp));
       init_kp = false;
     }
     Kp = 0.1*(double)encorder_count;
-    lcd.write_str("          ",LINE_3,4);
-    lcd.write_double(Kp,LINE_3,4);
+    if(flag_100ms && flag)
+    {
+      lcd.write_str("          ",LINE_3,4);
+      lcd.write_double(posiZ_pid.Kp,LINE_3,4);
+    }
     break;
   
   case 2:
     init_kp = true;
     init_kd = true;
-    if(init_ki){
-      lcd.write_str("Ki ",LINE_3,1); //3コマ使用
-      enc.setEncCount((int)(10.0 * Ki));
+    if(init_ki)
+    {
+      if(flag) lcd.write_str("Ki ",LINE_3,1); //3コマ使用
+      enc.setEncCount((int)(10.0 * posiZ_pid.Ki));
       init_ki = false;
     }
     Ki = 0.1*(double)encorder_count;
-    lcd.write_str("          ",LINE_3,4);
-    lcd.write_double(Ki,LINE_3,4);
+    if(flag_100ms && flag)
+    {
+      lcd.write_str("          ",LINE_3,4);
+      lcd.write_double(posiZ_pid.Ki,LINE_3,4);
+    }
     break;
   
   case 3:
     init_kp = true;
     init_ki = true;
-    if(init_kd){
-      lcd.write_str("Kd ",LINE_3,1); //3コマ使用
-      enc.setEncCount((int)(10.0 * Kd));
+    if(init_kd)
+    {
+      if(flag) lcd.write_str("Kd ",LINE_3,1); //3コマ使用
+      enc.setEncCount((int)(10.0 * posiZ_pid.Kd));
       init_kd = false;
     }
     Kd = 0.1*(double)encorder_count;
-    lcd.write_str("          ",LINE_3,4);
-    lcd.write_double(Kd,LINE_3,4);
+    if(flag_100ms && flag)
+    {
+      lcd.write_str("          ",LINE_3,4);
+      lcd.write_double(posiZ_pid.Kd,LINE_3,4);
+    }
     break;
 
   default:
     break;
   }
+  if(flag_100ms && flag)
+  {
+    lcd.write_str("        ",LINE_4,7);
+    lcd.write_double(serial_num,LINE_4,7);
+  }
   posiZ_pid.setPara(Kp,Ki,Kd);
 }
 
 // 最大最小範囲に収まるようにする関数
-double min_max(double value, double minmax){
+double min_max(double value, double minmax)
+{
   if(value > minmax) value = minmax;
   else if(value < -minmax) value = -minmax;
   return value;
 }
-
 /****割込みの関数****/
-void timer_warikomi(){
+void timer_warikomi()
+{
   DR.RGB_led(2);
   //DR.calcu_robotPosition();
   DR.calcu_roboAngle(); // calcu_robotPosition関数を使用する場合は不要
+  serial_num = ManualCon.updatePosiPID(globalVelZ,MAXOMEGA,DR.roboAngle,JOYCONPID);
 
   wall_1.wall_time_count(INT_TIME); // 壁越えの時間に関する処理
   wall_2.wall_time_count(INT_TIME); // ↓
@@ -192,11 +228,19 @@ void timer_warikomi(){
   button_RIGHT = button_right.button_fall();
   button_LEFT = button_left.button_fall();
 
+  static int count_100ms = 0;
+  if(10 <= count_100ms){
+    flag_100ms = true;
+    count_100ms = 0;
+  }
+  count_100ms++;
+
   flag_10ms = true;
 }
 
-void setup(){
-
+void setup()
+{
+  delay(1000);
   Serial.begin(115200);
   //SERIAL_LEONARDO.begin(9600);
   SERIAL_LCD.begin(115200);
@@ -205,26 +249,38 @@ void setup(){
   DR.BasicSetup();   //　汎用基板などの様々なセットアップを行う
   DR.allOutputLow(); //  出力をLOWにする
 
-  roboclaw.begin(115200);
-  SERIAL_XBEE.begin(115200);
+  roboclawR.begin(230400);
+  roboclawL.begin(230400);
+
+  Con.begin(115200);
   //Con.begin_api(115200); // apiでの通信
 
   lcd.clear_display();
   lcd.color_red();
-  lcd.write_str("waiting....",LINE_2,1);
+  lcd.write_str("please initialize",LINE_2,1);
   
   //ボードのスイッチが押されるまで待機
   bool ready_to_start = false;
-  while(!ready_to_start){
+  bool wall_init = false;
+  while(!ready_to_start)
+  {
     Con.update(PIN_LED_USER);
     //roboclawの原点出し
-    if(Con.readButton(BUTTON_PS) == PUSHED){  
-      roboclaw.ResetEncoders(ADR_MD_WHEE_1);
-      roboclaw.ResetEncoders(ADR_MD_WHEE_2);
-      roboclaw.ResetEncoders(ADR_MD_WHEE_3);
-      roboclaw.ResetEncoders(ADR_MD_WHEE_4);
+    if(Con.readButton(BUTTON_PS,PUSHE) || !digitalRead(PIN_SW))
+    {  
+      roboclawL.ResetEncoders(ADR_MD_WHEE_1);
+      roboclawL.ResetEncoders(ADR_MD_WHEE_2);
+      roboclawR.ResetEncoders(ADR_MD_WHEE_3);
+      roboclawR.ResetEncoders(ADR_MD_WHEE_4);
+      posiZ_pid.PIDinit(0.0,0.0);
+      DR.LEDblink(PIN_LED_GREEN, 2, 100);
+      lcd.clear_display();
+      lcd.write_str("push triangle",LINE_3,1);
+      wall_init = true;
     }
-    if(Con.getButtonState() & BUTTON_MARU || !digitalRead(PIN_SW)){
+
+    if((Con.readButton(BUTTON_SANKAKU,PUSHE) || !digitalRead(PIN_SW)) && wall_init)
+    {
       DR.LEDblink(PIN_LED_BLUE, 2, 100);
       lcd.clear_display();
       lcd.color_blue();
@@ -240,11 +296,10 @@ void setup(){
   MsTimer2::start();
 }
 
-void loop(){
-
+void loop()
+{
   Con.update(PIN_LED_USER); //コントローラの状態を更新
   dipSetup(); // ディップスイッチでの設定
-  if(turning_mode == 2 ) radianPID_setup(); // pidのゲインを設定
 /*
   uint8_t led_num = 0;
   while(SERIAL_LEONARDO.available()){
@@ -259,104 +314,170 @@ void loop(){
     }
   }
 */
-
   //展開前の状態に戻す
-  if(Con.readButton(BUTTON_PAD) == PUSHED){
+  if(Con.readButton(BUTTON_PAD,PUSHED))
+  {
     expand_right.init();
     expand_left.init();
+    wall_1.init();
+    wall_2.init();
+    wall_3.init();
+    wall_4.init();
   }
 
+  static int front_position_num = 1;
+  static int rear_position_num = 1;
   //roboclawの原点出し
-  if(Con.readButton(BUTTON_PS) == PUSHED){  
-    roboclaw.ResetEncoders(ADR_MD_WHEE_1);
-    roboclaw.ResetEncoders(ADR_MD_WHEE_2);
-    roboclaw.ResetEncoders(ADR_MD_WHEE_3);
-    roboclaw.ResetEncoders(ADR_MD_WHEE_4);
+  if(Con.readButton(BUTTON_PS,PUSHED))
+  {  
+    roboclawL.ResetEncoders(ADR_MD_WHEE_1);
+    roboclawL.ResetEncoders(ADR_MD_WHEE_2);
+    roboclawR.ResetEncoders(ADR_MD_WHEE_3);
+    roboclawR.ResetEncoders(ADR_MD_WHEE_4);
+    rear_position_num = 1;
+    front_position_num = 1;
   }
   
-  if( flag_10ms ){
+  //**10msの処理**//
+  if( flag_10ms )
+  {
+    if(turning_mode == 2 ) radianPID_setup(false); // pidのゲインを設定
 
-    expand_right.expand_func(Con.readButton(BUTTON_R1),expand_mode);
-    expand_left.expand_func(Con.readButton(BUTTON_L1),expand_mode);
+    //expand_right.expand_func(Con.readButton(BUTTON_R1),expand_mode); //展開右
+    //expand_left.expand_func(Con.readButton(BUTTON_L1),expand_mode);  //展開左
   
-    double Cx,Cy,Cz; //速度の倍数
-    Cx = 0.5, Cy = 0.5, Cz = 1.0;
+    static double Cx = 1.0;
+    static double Cy = 1.0;
+    static double Cz = 1.0; //速度の倍数
+    
+    if(Con.readButton(BUTTON_R2,PUSHED))
+    { 
+      if(Cx < 3.0) //上限は3倍(3.0m/s)
+      {
+        Cx += 0.5; 
+        Cy += 0.5;
+      }
+      lcd.clear_display();
+      lcd.write_double(Cx,LINE_3,3);
+    }
+    if(Con.readButton(BUTTON_L2,PUSHED))
+    {
+      if(0.5 < Cx) //上限は0.5倍(0.5m/s)
+      {
+        Cx -= 0.5;
+        Cy -= 0.5;
+      }
+      lcd.clear_display();
+      lcd.write_double(Cy,LINE_3,3);
+    }
+    
     coords gloabalVel = ManualCon.getGlobalVel(Con.LJoyX,Con.LJoyY,Con.RJoyY);
-    coords localVel = ManualCon.getLocalVel(Cx*gloabalVel.x, Cy*gloabalVel.y, Cz*gloabalVel.z, DR.roboAngle);
-    if(turning_mode == 1) localVel.z = gloabalVel.z; // 角度PIDをするかしないか
-    coords refV = ManualCon.getVel_max(localVel.x, localVel.y, localVel.z);
-    coords_4 VelCmd = ManualCon.getCmd(refV.x,refV.y,refV.z);
-  /*
-    roboclaw.SpeedM1(wall_1.adress,VelCmd.i);   // 足回りの速度指令
-    roboclaw.SpeedM1(wall_2.adress,VelCmd.ii);  // ↓
-    roboclaw.SpeedM1(wall_3.adress,VelCmd.iii); // ↓
-    roboclaw.SpeedM1(wall_4.adress,VelCmd.iv);  // ↓
-  */
+    globalVelZ = gloabalVel.z;
+    bool turningMode;
+    if(turning_mode == 1) turningMode = true;
+    else turningMode = true;
+    coords localVel = ManualCon.getLocalVel(Cx*gloabalVel.x, Cy*gloabalVel.y, Cz*gloabalVel.z, DR.roboAngle, JOYCONPID);
+    // coords refV = ManualCon.getVel_max(localVel.x, localVel.y, localVel.z);
+    // coords VelCmd = ManualCon.getCmd(refV.x,refV.y,refV.z);
+    coords_4 VelCmd = ManualCon.getCmd(localVel.x,localVel.y,localVel.z);
+
     wall_1.roboclawSpeedM1(VelCmd.i);   // 足回りの速度指令
     wall_2.roboclawSpeedM1(VelCmd.ii);  // ↓
     wall_3.roboclawSpeedM1(VelCmd.iii); // ↓
     wall_4.roboclawSpeedM1(VelCmd.iv);  // ↓
 
-    double robot_x_vel; //ロボットのx方向の移動速度
-    robot_x_vel = 0.5;
-    static double wall_rad = 0.0; //壁越え機構の回転角
-    static double pre_wall_rad = 0.0;
-    static double wall_seconds = 0.0;
-    if(Con.readButton(BUTTON_LEFT) == PUSHED) wall_rad = 0.0;
-    if(Con.readButton(BUTTON_UP) == PUSHED) wall_rad = 90.0;
-    if(Con.readButton(BUTTON_RIGHT) == PUSHED) wall_rad = 180.0;
-
-    if(fabs(pre_wall_rad - wall_rad) == 180.0) wall_seconds = 1.0;
-    else wall_seconds = 0.5;
+    // Serial.print(digitalRead(PIN_SW_WALL_1));
+    // Serial.print("\t");
+    // Serial.print(digitalRead(PIN_SW_WALL_2));
+    // Serial.print("\t");
+    // Serial.print(digitalRead(PIN_SW_WALL_3));
+    // Serial.print("\t");
+    // Serial.print(digitalRead(PIN_SW_WALL_4));
+    // Serial.print("\t");
+    // Serial.println(digitalRead(PIN_SW_RIGHT));
+    //Serial.print("\t");
+    //Serial.println(digitalRead(PIN_SW_LEFT));
     
+    double robot_x_vel = 1.0; //ロボットのx方向の移動速度, 1.0は積の単位元
+    robot_x_vel *= Cx;
+
+    static double wall_rad_F = 0.0; //壁越え機構の回転角
+    static double wall_rad_R = 0.0; //壁越え機構の回転角
+    static double refOmega_wall = 180.0; //壁越え機構の角速度[deg/s]
     switch (wall_mode)
     {
     case 1:
-    /*
-      if(wall_1.send_wall_position(wall_rad)){
-        roboclaw.SpeedAccelDeccelPositionM2(wall_1.adress,wall_1.accel,wall_1.omega,wall_1.accel,wall_1.position,true);
+      if(Con.readButton(BUTTON_RIGHT,PUSHE)) //倒立状態
+      {
+        rear_position_num = 0;
+        front_position_num = 0;
+        refOmega_wall = 180.0; // deg/s
       }
-      if(wall_2.send_wall_position(wall_rad)){
-        roboclaw.SpeedAccelDeccelPositionM2(wall_2.adress,wall_2.accel,wall_2.omega,wall_2.accel,wall_2.position,true);
+      if(Con.readButton(BUTTON_UP,PUSHE)) //初期状態（8輪接地）
+      {
+        rear_position_num = 1;
+        front_position_num = 1;
+        refOmega_wall = 180.0; // deg/s
+      } 
+      if(Con.readButton(BUTTON_LEFT,PUSHE)) //壁越え後の倒立状態
+      {
+        rear_position_num = 2;
+        front_position_num = 2;
+        refOmega_wall = 180.0; // deg/s
       }
-      if(wall_3.send_wall_position(wall_rad)){
-        roboclaw.SpeedAccelDeccelPositionM2(wall_3.adress,wall_3.accel,wall_3.omega,wall_3.accel,wall_3.position,true);
+      if(Con.readButton(BUTTON_DOWN,PUSHE)) //壁越え後の8輪接地
+      {
+        rear_position_num = 3;
+        front_position_num = 3;
+        refOmega_wall = 180.0; // deg/s
       }
-      if(wall_4.send_wall_position(wall_rad)){
-        roboclaw.SpeedAccelDeccelPositionM2(wall_4.adress,wall_4.accel,wall_4.omega,wall_4.accel,wall_4.position,true);
+      if(Con.readButton(BUTTON_R1,PUSHE))
+      {
+        front_position_num = 2; // 90[deg]
+        double refSeconds_wall; // 壁越えに必要な時間
+        refSeconds_wall = DISTANCE_WALL / robot_x_vel;
+        refOmega_wall = 180.0 / refSeconds_wall;
       }
-    */
-      wall_1.send_wall_position(wall_rad, wall_seconds);
-      wall_2.send_wall_position(wall_rad, wall_seconds);
-      wall_3.send_wall_position(wall_rad, wall_seconds);
-      wall_4.send_wall_position(wall_rad, wall_seconds);
-      break;
-    
-    case 2:
-    /*
-      if(wall_1.send_wall_cmd(180.0,robot_x_vel)){
-        roboclaw.SpeedAccelDeccelPositionM2(wall_1.adress,wall_1.accel,wall_1.omega,wall_1.accel,wall_1.position,true);
+      if(Con.readButton(BUTTON_L1,PUSHE))
+      {
+        rear_position_num = 2; // 90[deg]
+        double refSeconds_wall; // 壁越えに必要な時間
+        refSeconds_wall = DISTANCE_WALL / robot_x_vel;
+        refOmega_wall = 180.0 / refSeconds_wall;
       }
-      if(wall_2.send_wall_cmd(180.0,robot_x_vel)){
-        roboclaw.SpeedAccelDeccelPositionM2(wall_2.adress,wall_2.accel,wall_2.omega,wall_2.accel,wall_2.position,true);
-      }
-      if(wall_3.send_wall_cmd(180.0,robot_x_vel)){
-        roboclaw.SpeedAccelDeccelPositionM2(wall_3.adress,wall_3.accel,wall_3.omega,wall_3.accel,wall_3.position,true);
-      }
-      if(wall_4.send_wall_cmd(180.0,robot_x_vel)){
-        roboclaw.SpeedAccelDeccelPositionM2(wall_4.adress,wall_4.accel,wall_4.omega,wall_4.accel,wall_4.position,true);
-      }
-    */
-      wall_1.send_wall_cmd(180.0,robot_x_vel);
-      wall_2.send_wall_cmd(180.0,robot_x_vel);
-      wall_3.send_wall_cmd(180.0,robot_x_vel);
-      wall_4.send_wall_cmd(180.0,robot_x_vel);
-      break;
 
-    default:
-      break;
+      switch (front_position_num)
+      {
+      case 0: wall_rad_F = -90.0; break; //倒立状態
+      case 1: wall_rad_F = 0.0;   break; //初期状態（8輪接地）
+      case 2: wall_rad_F = 90.0;  break; //壁越え後の倒立状態
+      case 3: wall_rad_F = 180.0; break; //壁越え後の8輪接地
+      default:
+        break;
+      }
+    
+      switch (rear_position_num)
+      {
+      case 0: wall_rad_R = -90.0; break; //倒立状態
+      case 1: wall_rad_R = 0.0;   break; //初期状態（8輪接地）
+      case 2: wall_rad_R = 90.0;  break; //壁越え後の倒立状態
+      case 3: wall_rad_R = 180.0; break; //壁越え後の8輪接地
+      default:
+        break;
+      }
+      break; // wall_mode == 1　のブレイク
+      default:
+       break;
     }
+    wall_1.send_wall_position(wall_rad_F, refOmega_wall);
+    wall_2.send_wall_position(wall_rad_R, refOmega_wall);
+    wall_3.send_wall_position(wall_rad_R, refOmega_wall);
+    wall_4.send_wall_position(wall_rad_F, refOmega_wall);
 
     flag_10ms = false;
+  }
+  if(flag_100ms)
+  {
+    flag_100ms = false;
   }
 }
